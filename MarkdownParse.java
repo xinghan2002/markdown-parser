15218b3bd746411944f8bb3027ce9cd4d0b14bde
xinghan2002 <wanghui4856@gmail.com>
2022-04-14 18:56:37 -0700

    no

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,18 +11,23 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
+
+            if(openBracket == -1) break;
+
+            while(markdown.charAt(openBracket - 1) == '!'){
+                openBracket = markdown.indexOf("[", openBracket + 1);
+            }
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
 
-            if(openBracket == -1){
-                break;
-            }else{
+            
             currentIndex = closeParen + 1;
-            }
+            
         }
 
         return toReturn;
--- test-file3.md
+++ test-file3.md
@@ -1,6 +1,6 @@
-# Title
-
 [link1](https://something.com)
 [link2](some-thing.html)
 ![image1](some-thing.html)
+[link3](some-thing.html)
+[link
 [link3](some-thing.html)
