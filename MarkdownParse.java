3b1a189e86927365e5954d4524b4de8f67d8032a
kcyy127 <66866365+kcyy127@users.noreply.github.com>
2022-04-14 19:03:57 -0700

    fixed both test-file2 and test-file3

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -24,6 +24,7 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
 
+            if (openParen == -1 || closeParen == -1) break;
 
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
