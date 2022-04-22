ec311d9aa69fc459f15c67081e82d554f5aeeb26
kcyy127 <66866365+kcyy127@users.noreply.github.com>
2022-04-21 18:53:06 -0700

    First successfully run JUnit test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,30 @@
 import static org.junit.Assert.*;
-import org.junit.*;
+
+import java.util.ArrayList;
+    import java.util.Arrays;
+
+    import org.junit.*;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class MarkdownParseTest {
 
+    // @Test
+    // public void addition() {
+    //     assertEquals(2, 1 + 1);
+    // }
+
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void testFile1() throws IOException {
+
+        String content = Files.readString(Path.of("test-file.md"));
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> result = new ArrayList<>(Arrays.asList("https://something.com", "some-thing.html"));
+
+        assertArrayEquals(result.toArray(), links.toArray());
     }
 }
